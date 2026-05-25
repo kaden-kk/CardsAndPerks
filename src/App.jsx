0eca8894c9acacf9003eb3de35d76e0258fcc3cb
Kaden <kadenkwan.kk@gmail.com>
2026-05-25 16:29:50 -0400

    Database Setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -118,5 +118,3 @@ function App() {
     </>
   )
 }
-
-export default App
